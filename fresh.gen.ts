b76aa752277712dabc8166577c62b42e4e1d7751
person0b <in0bbox@gmail.com>
2023-12-23 01:31:11 +0400

    fix: update fresh manifest

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,35 +2,27 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/[placeId].tsx";
-import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/api/botfather.ts";
-import * as $3 from "./routes/api/joke.ts";
-import * as $4 from "./routes/index.tsx";
-import * as $5 from "./routes/places/[id]/index.tsx";
-import * as $6 from "./routes/places/add.tsx";
-import * as $7 from "./routes/places/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
-import * as $$1 from "./islands/InitializeWebApp.tsx";
-import * as $$2 from "./islands/TryWebApp.tsx";
+import * as $0 from './routes/[placeId].tsx'
+import * as $1 from './routes/_app.tsx'
+import * as $2 from './routes/api/botfather.ts'
+import * as $3 from './routes/index.tsx'
+import * as $$0 from './islands/Counter.tsx'
+import * as $$1 from './islands/InitializeWebApp.tsx'
+import * as $$2 from './islands/TryWebApp.tsx'
 
 const manifest = {
   routes: {
-    "./routes/[placeId].tsx": $0,
-    "./routes/_app.tsx": $1,
-    "./routes/api/botfather.ts": $2,
-    "./routes/api/joke.ts": $3,
-    "./routes/index.tsx": $4,
-    "./routes/places/[id]/index.tsx": $5,
-    "./routes/places/add.tsx": $6,
-    "./routes/places/index.tsx": $7,
+    './routes/[placeId].tsx': $0,
+    './routes/_app.tsx': $1,
+    './routes/api/botfather.ts': $2,
+    './routes/index.tsx': $3,
   },
   islands: {
-    "./islands/Counter.tsx": $$0,
-    "./islands/InitializeWebApp.tsx": $$1,
-    "./islands/TryWebApp.tsx": $$2,
+    './islands/Counter.tsx': $$0,
+    './islands/InitializeWebApp.tsx': $$1,
+    './islands/TryWebApp.tsx': $$2,
   },
   baseUrl: import.meta.url,
-};
+}
 
-export default manifest;
+export default manifest
