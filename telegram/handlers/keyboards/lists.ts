a12615f226f2c2eb2d9115c065763f435db94949
person0b <in0bbox@gmail.com>
2023-12-24 18:27:48 +0400

    fix: lists' layout

--- telegram/handlers/keyboards/lists.ts
+++ telegram/handlers/keyboards/lists.ts
@@ -13,7 +13,7 @@ export const createCountryListKeyboard = (countries: string[]) => {
   const keyboard = new InlineKeyboard()
 
   for (const country of countries) {
-    keyboard.text(country, `${ListKeyboardKey.Country}:${country}`)
+    keyboard.text(country, `${ListKeyboardKey.Country}:${country}`).row()
   }
 
   return keyboard
@@ -23,7 +23,7 @@ export const createCityListKeyboard = (cities: string[]) => {
   const keyboard = new InlineKeyboard()
 
   for (const city of cities) {
-    keyboard.text(city, `${ListKeyboardKey.City}:${city}`)
+    keyboard.text(city, `${ListKeyboardKey.City}:${city}`).row()
   }
 
   return keyboard
@@ -33,7 +33,7 @@ export const createLocalityListKeyboard = (localities: string[]) => {
   const keyboard = new InlineKeyboard()
 
   for (const locality of localities) {
-    keyboard.text(locality, `${ListKeyboardKey.Locality}:${locality}`)
+    keyboard.text(locality, `${ListKeyboardKey.Locality}:${locality}`).row()
   }
 
   return keyboard
@@ -43,7 +43,7 @@ export const createPlacesListKeyboard = (places: PlaceListData[]) => {
   const keyboard = new InlineKeyboard()
 
   for (const place of places) {
-    keyboard.text(place.title, `${PlaceKeyboardKey.Show}:${place.id}`)
+    keyboard.text(place.title, `${PlaceKeyboardKey.Show}:${place.id}`).row()
   }
 
   return keyboard
